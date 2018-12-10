dd9f7e13d65bb047516416068c2cbdbf7f925028
Mark Kamuda <kamuda1@illinois.edu>
2018-12-10 16:26:23 -0600

    Update test_training.py

--- annsa/tests/test_training.py
+++ annsa/tests/test_training.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import LabelBinarizer, FunctionTransformer
 from sklearn.pipeline import make_pipeline
 
 from annsa.model_classes import dnn_model_features, DNN
-# from annsa.results_plotting_functions import hyperparameter_efficiency_plot
+from annsa.results_plotting_functions import hyperparameter_efficiency_plot
 
 tf.enable_eager_execution()
 
@@ -18,14 +18,28 @@ def load_dataset():
     training_dataset = make_classification(n_samples=100,
                                            n_features=1024,
                                            n_informative=200,
-                                           n_classes=29)
+                                           n_classes=2)
 
     testing_dataset = make_classification(n_samples=100,
                                           n_features=1024,
                                           n_informative=200,
-                                          n_classes=29)
+                                          n_classes=2)
 
-    return training_dataset, testing_dataset
+    mlb = LabelBinarizer()
+
+    training_data = np.abs(training_dataset[0])
+    training_keys = training_dataset[1]
+    training_keys_binarized = mlb.fit_transform(
+        training_keys.reshape([training_data.shape[0], 1]))
+    train_dataset = [training_data, training_keys_binarized]
+
+    testing_data = np.abs(testing_dataset[0])
+    testing_keys = testing_dataset[1]
+    testing_keys_binarized = mlb.transform(
+        testing_keys.reshape([testing_data.shape[0], 1]))
+    test_dataset = [testing_data, testing_keys_binarized]
+
+    return train_dataset, test_dataset
 
 
 def test_dnn_training():
@@ -33,37 +47,23 @@ def test_dnn_training():
     Testing the dense neural network class and training function.
     """
     scaler = make_pipeline(FunctionTransformer(np.log1p, validate=False))
-    mlb = LabelBinarizer()
-
-    training_dataset, testing_dataset = load_dataset()
-    training_data = np.abs(training_dataset[0])
-    training_keys = training_dataset[1]
-    training_keys_binarized = mlb.fit_transform(
-        training_keys.reshape([training_data.shape[0], 1]))
-    train_dataset = [training_data, training_keys_binarized]
-    
-    testing_data = np.abs(testing_dataset[0])
-    testing_keys = testing_dataset[1]
-    testing_keys_binarized = mlb.transform(
-        testing_keys.reshape([testing_data.shape[0], 1]))
-    test_dataset = [testing_data, testing_keys_binarized]
 
     model_features = dnn_model_features(
         learining_rate=1e-1,
         l2_regularization_scale=1e-1,
         dropout_probability=0.5,
         batch_size=2**5,
-        output_size=29,
-        dense_nodes=[200, 100, 50],
+        output_size=2,
+        dense_nodes=[50],
         scaler=scaler
         )
 
-    model_features.scaler.fit(training_data)
-    test_dataset = (testing_data, testing_keys_binarized)
+    train_dataset, test_dataset = load_dataset()
+    model_features.scaler.fit(train_dataset[0])
+
     def data_augmentation(input_data):
-        return np.random.poisson(input_data).astype(float)
-    
-    
+        return input_data
+
     tf.reset_default_graph()
     optimizer = tf.train.AdamOptimizer(model_features.learining_rate)
     model = DNN(model_features)
@@ -95,12 +95,12 @@ def test_ae_training():
     """
     pass
 
-#  def test_hyperparameter_efficiency_plot():
-#     """
-#     Testing the hyperparameter_efficiency_plot function with a test
-#     data vector.
-#     """
-#     accuracy = np.random.normal(0.8,0.2,64)
-#     _, _ = hyperparameter_efficiency_plot(accuracy)
-#     pass
 
+def test_hyperparameter_efficiency_plot():
+    """
+    Testing the hyperparameter_efficiency_plot function with a test
+    data vector.
+    """
+    accuracy = np.random.normal(0.8, 0.2, 64)
+    _, _ = hyperparameter_efficiency_plot(accuracy)
+    pass
