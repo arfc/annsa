f386e98749c26e52aec9623bd658b82d5c35d67c
Mark Kamuda <ubuntu@ip-172-31-36-219.us-west-2.compute.internal>
2019-04-10 19:51:27 +0000

    adds load_trained_model function

--- annsa/load_pretrained_network.py
+++ annsa/load_pretrained_network.py
@@ -47,6 +47,39 @@ def load_features(filename):
     return model_features
 
 
+def load_trained_model(model_class,
+                       features_filename,
+                       weights_filename,
+                       ):
+    '''
+    Loads a trained model.
+
+    inputs:
+        model_class : Keras model class
+            The keras model class for the features
+        features_filename : str
+            Filname location of the pickle file containing the model features
+        weights_filename : str
+            Filname location of the file containing the pretrained weights
+
+    outputs:
+        model : Keras model class
+            The class containing the keras model with pretrained weights
+        model_features : model features class
+            The class containing the model features.
+    '''
+
+    model_features = load_features(features_filename)
+    trained_model = model_class(model_features)
+    trained_model.load_weights(weights_filename)
+
+    # need to do a forward pass to initialize weights
+    dummy_data = np.zeros(1024)
+    _ = trained_model.predict_class([dummy_data])
+
+    return trained_model, model_features
+
+
 def load_pretrained_cae_into_cnn(cae_features_filename,
                                  cae_weights_filename,
                                  cnn_dense_nodes=[128],
