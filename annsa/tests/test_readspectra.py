dc563875945bd5637ef6d6171fa6f72e3f771077
Mark Kamuda <kamuda1@illinois.edu>
2019-08-21 14:35:09 -0400

    Update annsa/tests/test_readspectra.py
    Co-Authored-By: Katy Huff <katyhuff@users.noreply.github.com>

--- annsa/tests/test_readspectra.py
+++ annsa/tests/test_readspectra.py
@@ -2,7 +2,7 @@ import os
 from annsa.annsa import read_spectrum
 
 
-def test_read_spectrum1():
+def test_read_spectrum_rocky_flats():
     TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), './data_folder/rocky_flats_spectra.spe')
     _ = read_spectrum(TESTDATA_FILENAME)
     pass
