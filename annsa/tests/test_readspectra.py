cd3656560931d4d56b4b158fded6ccdd510f20ae
Mark Kamuda <ubuntu@ip-172-31-36-219.us-west-2.compute.internal>
2019-08-21 19:22:13 +0000

    adds list equal asserts

--- annsa/tests/test_readspectra.py
+++ annsa/tests/test_readspectra.py
@@ -1,14 +1,23 @@
 import os
+from numpy import load
 from annsa.annsa import read_spectrum
 
 
 def test_read_spectrum_rocky_flats():
-    TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), './data_folder/rocky_flats_spectra.spe')
-    _ = read_spectrum(TESTDATA_FILENAME)
+    truedata_filename = os.path.join(os.path.dirname(__file__), './data_folder/rocky_flats.npy')
+    true_spectrum = load(truedata_filename)
+
+    annsa_data_filename = os.path.join(os.path.dirname(__file__), './data_folder/rocky_flats_spectra.spe')
+    annsa_spectrum = read_spectrum(annsa_data_filename)
+    assert (annsa_spectrum == true_spectrum).all()
     pass
 
 
 def test_read_spectrum_gadras_template():
-    TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), './data_folder/gadras_template.spe')
-    _ = read_spectrum(TESTDATA_FILENAME, float)
+    truedata_filename = os.path.join(os.path.dirname(__file__), './data_folder/gadras_template.npy')
+    true_spectrum = load(truedata_filename)
+
+    annsa_data_filename = os.path.join(os.path.dirname(__file__), './data_folder/gadras_template.spe')
+    annsa_spectrum = read_spectrum(annsa_data_filename, float)
+    assert (annsa_spectrum == true_spectrum).all()
     pass
