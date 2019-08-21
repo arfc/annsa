95d6f379234a3b5aa27f7f5b5072bf5106406ff0
Mark Kamuda <ubuntu@ip-172-31-36-219.us-west-2.compute.internal>
2019-08-21 01:05:21 +0000

    updates read_spectra function

---
+++ annsa/tests/test_readspectra.py
@@ -0,0 +1,13 @@
+import os
+from annsa.annsa import read_spectrum
+
+
+def test_read_spectrum():
+    TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), './data_folder/rocky_flats_spectra.spe')
+    _ = read_spectrum(TESTDATA_FILENAME)
+    pass
+
+def test_read_spectrum():
+    TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), './data_folder/gadras_template.spe')
+    _ = read_spectrum(TESTDATA_FILENAME, float)
+    pass
