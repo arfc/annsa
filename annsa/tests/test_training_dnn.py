b8ad79504e83cb39b91392bdbeea35fad309f077
Mark Kamuda <ubuntu@ip-172-31-36-219.us-west-2.compute.internal>
2019-09-18 23:29:28 +0000

    updated list comp

--- annsa/tests/test_training_dnn.py
+++ annsa/tests/test_training_dnn.py
@@ -35,12 +35,8 @@ def dnn(request):
     # forward pass to initialize dnn weights
     model.forward_pass(np.ones([1, input_size]), training=False)
     # set weights to ones
-    weight_ones = []
-    for index, weight in enumerate(model.get_weights()):
-        if index % 2 == 0:
-            weight_ones.append(np.ones(weight.shape))
-        else:
-            weight_ones.append(weight)
+    weight_ones = [np.ones(weight.shape) if (index % 2 == 0) else weight for index,
+                   weight in enumerate(model.get_weights())]
     model.set_weights(weight_ones)
     return model
 
