803643bcc633eb23440a0e74dce8a7dce26ec780
Mark Kamuda <ubuntu@ip-172-31-36-219.us-west-2.compute.internal>
2019-09-18 18:52:39 +0000

    adds training tests

--- annsa/tests/test_training_cnn1d.py
+++ annsa/tests/test_training_cnn1d.py
@@ -2,24 +2,26 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import tensorflow as tf
 import pytest
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer, LabelBinarizer
 from sklearn.pipeline import make_pipeline
+from sklearn.datasets import load_digits
 from annsa.model_classes import (generate_random_cnn1d_architecture,
                                  CNN1D)
 
 tf.enable_eager_execution()
 
 
-@pytest.fixture(params=[([],  0.5,),
-                        ([],  0.999,),
-                        ([10], 0.999)])
+@pytest.fixture(params=[([],  0.5, 64),
+                        ([],  0.5, 1024),
+                        ([],  0.999, 1024),
+                        ([10], 0.999, 1024),])
 def cnn1d(request):
     '''
     Constructs a convolutional neural network with filters
     initialized to ones. Fixture params are either zero or one hidden
     dense layer.
     '''
-    dense_nodes, dropout_probability = request.param
+    (dense_nodes, dropout_probability, input_size) = request.param
 
     scaler = make_pipeline(FunctionTransformer(np.abs, validate=False))
     model_features = generate_random_cnn1d_architecture(
@@ -28,8 +30,8 @@ def cnn1d(request):
         pool_size_choices=((4, ), ))
     model_features.learning_rate = 1e-1
     model_features.trainable = True
-    model_features.batch_size = 2**5
-    model_features.output_size = 2
+    model_features.batch_size = 5
+    model_features.output_size = 3
     model_features.output_function = None
     model_features.l2_regularization_scale = 1e1
     model_features.dropout_probability = dropout_probability
@@ -39,7 +41,7 @@ def cnn1d(request):
     model_features.dense_nodes = dense_nodes
     model = CNN1D(model_features)
     # forward pass to initialize cnn1d weights
-    model.forward_pass(np.ones([1, 1024]), training=False)
+    model.forward_pass(np.ones([1, input_size]), training=False)
     # set weights to ones
     weight_ones = []
     for index, weight in enumerate(model.get_weights()):
@@ -51,14 +53,32 @@ def cnn1d(request):
     return model
 
 
+@pytest.fixture()
+def toy_dataset():
+    '''
+    Constructs toy dataset of digits.
+    '''
+    data, target = load_digits(n_class=3, return_X_y=True)
+    mlb = LabelBinarizer()
+    targets_binarized = mlb.fit_transform(target)
+    return (data, targets_binarized)
+
+
 # forward pass tests
+@pytest.mark.parametrize('cnn1d',
+                         (([],  0.5, 1024),
+                         ([],  0.999, 1024),
+                         ([10], 0.999, 1024)),
+                         indirect=True,)
 def test_forward_pass_0(cnn1d):
     '''case 0: test if output size is correct'''
     output = cnn1d.forward_pass(np.ones([1, 1024]), training=False)
-    assert(output.shape[1] == 2)
+    assert(output.shape[1] == 3)
 
 
-@pytest.mark.parametrize('cnn1d', [([], 0.5)], indirect=True,)
+@pytest.mark.parametrize('cnn1d',
+                         (([],  0.5, 1024),),
+                         indirect=True,)
 def test_forward_pass_1(cnn1d):
     '''case 1: Tests response to a spectrum of all ones
     when weight filters are all one. Note, layer before output has an
@@ -70,26 +90,30 @@ def test_forward_pass_1(cnn1d):
 
 
 # loss function tests
-@pytest.mark.parametrize('cnn1d', [([], 0.5)], indirect=True,)
+@pytest.mark.parametrize('cnn1d',
+                         (([],  0.5, 1024),),
+                         indirect=True,)
 def test_loss_fn_0(cnn1d):
     '''case 0: tests if l2 regularization does not add to the loss_fn
     with hidden dense layers.'''
     loss = cnn1d.loss_fn(
         input_data=np.ones([1, 1024]),
-        targets=np.array([[16384, 16384]]),
+        targets=np.array([[16384, 16384, 16384]]),
         cost=cnn1d.mse,
         training=False)
     loss = loss.numpy()
     assert(loss == 0.)
 
 
-@pytest.mark.parametrize('cnn1d', [([10], 0.5)], indirect=True,)
+@pytest.mark.parametrize('cnn1d',
+                         (([10], 0.999, 1024),),
+                         indirect=True,)
 def test_loss_fn_1(cnn1d):
     '''case 1: tests if l2 regularization adds to loss_fn when there are
     dense hidden layers.'''
     loss = cnn1d.loss_fn(
         input_data=np.ones([1, 1024]),
-        targets=np.array([[16384, 16384]]),
+        targets=np.array([[16384, 16384, 16384]]),
         cost=cnn1d.mse,
         training=False)
     loss = loss
@@ -97,7 +121,9 @@ def test_loss_fn_1(cnn1d):
 
 
 # dropout test
-@pytest.mark.parametrize('cnn1d', [([], 0.999)], indirect=True,)
+@pytest.mark.parametrize('cnn1d',
+                         (([],  0.999, 1024),),
+                         indirect=True,)
 def test_dropout_0(cnn1d):
     '''case 0: tests that dropout is not applied when there are no dense
     hidden layers.'''
@@ -108,7 +134,9 @@ def test_dropout_0(cnn1d):
     assert(np.array_equal(o_training_false, o_training_true))
 
 
-@pytest.mark.parametrize('cnn1d', [([10], 0.999)], indirect=True,)
+@pytest.mark.parametrize('cnn1d',
+                         (([10], 0.999, 1024),),
+                         indirect=True,)
 def test_dropout_1(cnn1d):
     '''case 1: tests that dropout is applied when there are
     dense hidden layers'''
@@ -119,7 +147,9 @@ def test_dropout_1(cnn1d):
     assert(np.array_equal(o_training_false, o_training_true) is False)
 
 
-@pytest.mark.parametrize('cnn1d', [([10], 0.999)], indirect=True,)
+@pytest.mark.parametrize('cnn1d',
+                         (([10], 0.999, 1024),),
+                         indirect=True,)
 def test_dropout_2(cnn1d):
     '''case 2: tests that dropout is not applied during inference, when
     training is False.'''
@@ -128,3 +158,25 @@ def test_dropout_2(cnn1d):
     o_training_false_2 = cnn1d.forward_pass(np.ones([1, 1024]),
                                             training=False).numpy()
     assert(np.array_equal(o_training_false_1, o_training_false_2))
+
+
+# training tests
+@pytest.mark.parametrize('cost', ['mse', 'cross_entropy'])
+@pytest.mark.parametrize('cnn1d',
+                         (([],  0.5, 64),),
+                         indirect=True,)
+def test_training_0(cnn1d, toy_dataset, cost):
+    '''case 0: test if training on toy dataset reduces errors using 
+    both error functions'''
+    (data, targets_binarized) = toy_dataset
+    cost_function = getattr(cnn1d, cost)
+    objective_cost, earlystop_cost = cnn1d.fit_batch(
+        (data, targets_binarized),
+        (data, targets_binarized),
+        optimizer=tf.train.AdamOptimizer(1e-3),
+        num_epochs=2,
+        obj_cost=cost_function,
+        data_augmentation=cnn1d.default_data_augmentation,)
+    epoch0_error = objective_cost['test'][0].numpy()
+    epoch1_error = objective_cost['test'][1].numpy()
+    assert(epoch1_error < epoch0_error)
--- annsa/tests/test_training_dnn.py
+++ annsa/tests/test_training_dnn.py
@@ -2,35 +2,37 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import tensorflow as tf
 import pytest
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer, LabelBinarizer
 from sklearn.pipeline import make_pipeline
+from sklearn.datasets import load_digits
 from annsa.model_classes import DNN, dnn_model_features
+
 
 tf.enable_eager_execution()
 
-@pytest.fixture(params=[0.5, 0.999])
+@pytest.fixture(params=[(0.5, 1024),
+                        (0.5, 64), 
+                        (0.999, 1024)])
 def dnn(request):
     '''
     Constructs a dense neural network with dense connections
     initialized to ones.
     '''
-
-    dropout_probability = request.param
-
+    (dropout_probability, input_size) = request.param
     scaler = make_pipeline(FunctionTransformer(np.abs, validate=False))
     model_features = dnn_model_features(
-        learning_rate=1e-1,
+        learning_rate=1e-2,
         l2_regularization_scale=1e-1,
         dropout_probability=dropout_probability,
-        batch_size=2**5,
-        output_size=2,
+        batch_size=5,
+        output_size=3,
         dense_nodes=[10],
         output_function=None,
         activation_function=None,
         scaler=scaler)
     model = DNN(model_features)
     # forward pass to initialize dnn weights
-    model.forward_pass(np.ones([1, 1024]), training=False)
+    model.forward_pass(np.ones([1, input_size]), training=False)
     # set weights to ones
     weight_ones = []
     for index, weight in enumerate(model.get_weights()):
@@ -41,15 +43,29 @@ def dnn(request):
     model.set_weights(weight_ones)
     return model
 
+@pytest.fixture()
+def toy_dataset():
+    '''
+    Constructs toy dataset of digits.
+    '''
+    data, target = load_digits(n_class=3, return_X_y=True)
+    mlb = LabelBinarizer()
+    targets_binarized = mlb.fit_transform(target)
+    return (data, targets_binarized)
 
 # forward pass tests
+@pytest.mark.parametrize('dnn',
+                         ((0.5, 1024), (0.999, 1024)),
+                         indirect=True,)
 def test_forward_pass_0(dnn):
     '''case 0: Tests if output size is correct'''
     output = dnn.forward_pass(np.ones([1, 1024]), training=False)
-    assert(output.shape[1] == 2)
+    assert(output.shape[1] == 3)
 
 
-@pytest.mark.parametrize('dnn', [[]], indirect=True,)
+@pytest.mark.parametrize('dnn',
+                         ((0.5, 1024), (0.999, 1024)),
+                         indirect=True,)
 def test_forward_pass_1(dnn):
     '''case 1: Tests response to a spectrum of all ones
     when weight filters are all one. Note, layer before output has
@@ -61,11 +77,14 @@ def test_forward_pass_1(dnn):
 
 
 # loss function tests
+@pytest.mark.parametrize('dnn',
+                         ((0.5, 1024), (0.999, 1024)),
+                         indirect=True,)
 def test_loss_fn_0(dnn):
     '''case 0: tests if l2 regularization adds to loss_fn.'''
     loss = dnn.loss_fn(
         input_data=np.ones([1, 1024]),
-        targets=np.array([[16384, 16384]]),
+        targets=np.array([[16384, 16384, 16384]]),
         cost=dnn.mse,
         training=False)
     loss = loss
@@ -73,7 +92,9 @@ def test_loss_fn_0(dnn):
 
 
 # dropout tests
-@pytest.mark.parametrize('dnn', [0.999], indirect=True,)
+@pytest.mark.parametrize('dnn',
+                         ((0.999, 1024),),
+                         indirect=True,)
 def test_dropout_0(dnn):
     '''case 0: tests that dropout is applied when training.'''
     o_training_false = dnn.forward_pass(np.ones([1, 1024]),
@@ -83,7 +104,9 @@ def test_dropout_0(dnn):
     assert(np.array_equal(o_training_false, o_training_true) is False)
 
 
-@pytest.mark.parametrize('dnn', [0.999], indirect=True,)
+@pytest.mark.parametrize('dnn',
+                         ((0.999, 1024),),
+                         indirect=True,)
 def test_dropout_1(dnn):
     '''case 1: tests that dropout is not applied in inference, when training
     is False.'''
@@ -95,26 +118,22 @@ def test_dropout_1(dnn):
 
 
 # training tests
-@pytest.mark.parametrize('dnn', [0.5], indirect=True,)
-def test_training_0(dnn):
-    '''case 0: training on toy dataset reduces errors'''
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@pytest.mark.parametrize('cost', ['mse', 'cross_entropy'])
+@pytest.mark.parametrize('dnn',
+                         ((0.5, 64),),
+                         indirect=True,)
+def test_training_0(dnn, toy_dataset, cost):
+    '''case 0: test if training on toy dataset reduces errors using 
+    both error functions'''
+    (data, targets_binarized) = toy_dataset
+    cost_function = getattr(dnn, cost)
+    objective_cost, earlystop_cost = dnn.fit_batch(
+        (data, targets_binarized),
+        (data, targets_binarized),
+        optimizer=tf.train.AdamOptimizer(1e-2),
+        num_epochs=2,
+        obj_cost=cost_function,
+        data_augmentation=dnn.default_data_augmentation,)
+    epoch0_error = objective_cost['test'][0].numpy()
+    epoch1_error = objective_cost['test'][1].numpy()
+    assert(epoch1_error < epoch0_error)
